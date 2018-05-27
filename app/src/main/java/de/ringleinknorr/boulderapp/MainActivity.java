39607f165c54d13225ec1a85604cf1bf36dfc9ab
visualJ <benedikt-ringlein@hotmail.de>
2018-05-27 17:06:35 +0200

    Add timeline fragment and navigation host

--- app/src/main/java/de/ringleinknorr/boulderapp/MainActivity.java
+++ app/src/main/java/de/ringleinknorr/boulderapp/MainActivity.java
@@ -3,6 +3,8 @@ package de.ringleinknorr.boulderapp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import androidx.navigation.Navigation;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -10,4 +12,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return Navigation.findNavController(this, R.id.nav_host_fragment).navigateUp();
+    }
+
 }
