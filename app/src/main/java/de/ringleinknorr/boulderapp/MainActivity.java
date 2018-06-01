0e17dd1fc040b9198cbfb6049a98617fa9365c2b
Jonas Knorr <sieg.tronje@gmail.com>
2018-06-01 23:10:54 +0200

    replace default appbar with toolbar

--- app/src/main/java/de/ringleinknorr/boulderapp/MainActivity.java
+++ app/src/main/java/de/ringleinknorr/boulderapp/MainActivity.java
@@ -3,6 +3,7 @@ package de.ringleinknorr.boulderapp;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -11,22 +12,25 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.nav_drawer)
+    NavigationView navDrawer;
+    @BindView(R.id.app_toolbar)
+    Toolbar appToolbar;
     private NavController navController;
-    @BindView(R.id.nav_drawer) NavigationView navDrawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        setSupportActionBar(appToolbar);
+
         navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupWithNavController(navDrawer, navController);
-
     }
 
     @Override
     public boolean onSupportNavigateUp() {
         return navController.navigateUp();
     }
-
 }
