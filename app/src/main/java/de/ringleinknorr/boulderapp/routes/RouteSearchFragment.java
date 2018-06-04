2bd924fb2c3a9733abdbf796163dbe5d1a2b3753
Jonas Knorr <sieg.tronje@gmail.com>
2018-06-04 02:33:33 +0200

    change route search fragment style

--- app/src/main/java/de/ringleinknorr/boulderapp/routes/RouteSearchFragment.java
+++ app/src/main/java/de/ringleinknorr/boulderapp/routes/RouteSearchFragment.java
@@ -1,13 +1,16 @@
 package de.ringleinknorr.boulderapp.routes;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.ImageView;
 
 import java.util.Objects;
 
@@ -20,11 +23,14 @@ public class RouteSearchFragment extends Fragment {
     @BindView(R.id.auto_complete_text_view)
     AutoCompleteTextView
             autoCompleteTextView;
+    @BindView(R.id.gym_sector_view)
+    ImageView gymSectorImageView;
 
     public RouteSearchFragment() {
         // Required empty public constructor
     }
 
+    @SuppressLint({"ClickableViewAccessibility"})
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -36,11 +42,21 @@ public class RouteSearchFragment extends Fragment {
 
         AutoCompleteTextView textView = autoCompleteTextView;
         textView.setAdapter(adapter);
-        // Inflate the layout for this fragment
+        gymSectorImageView.setOnTouchListener((v, event) -> {
+            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                // Select sector
+            }
+            return true;
+        });
+
+
         return view;
     }
 
     private static final String[] HALLEN = new String[]{
             "Nordwand Wiesbaden", "Boulderwelt Frankfurt", "Blockwerk Mainz"
+    };
+    private static final Integer[][] COORDS = new Integer[][]{
+            {0, 0, 100, 100}
     };
 }
