a394249828fcbff3d83c832a983cb65135a14019
visualJ <benedikt-ringlein@hotmail.de>
2018-06-17 11:04:41 +0200

    Tweak selected icon alpha for better visibility on images

--- app/src/main/java/de/ringleinknorr/boulderapp/routes/RouteCardView.java
+++ app/src/main/java/de/ringleinknorr/boulderapp/routes/RouteCardView.java
@@ -21,7 +21,7 @@ public class RouteCardView extends ConstraintLayout implements SelectableItemLis
 
     public final float SELECTED_ICON_APLHA = 1;
     public final float SELECTED_ICON_SCALE = 1;
-    public final float DESELECTED_ICON_APLHA = 0.2f;
+    public final float DESELECTED_ICON_APLHA = 0.3f;
     public final float DESELECTED_ICON_SCALE = 0.8f;
 
     @BindView(R.id.route_level_text)
