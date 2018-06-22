1c555c2cb89035a2dc6d481b9f17385d7d7a622b
visualJ <benedikt-ringlein@hotmail.de>
2018-06-22 12:11:03 +0200

    Add result field to session route

--- app/src/main/java/de/ringleinknorr/boulderapp/timeline/SessionRoute.java
+++ app/src/main/java/de/ringleinknorr/boulderapp/timeline/SessionRoute.java
@@ -1,26 +1,35 @@
 package de.ringleinknorr.boulderapp.timeline;
 
+import android.support.annotation.NonNull;
+
 import de.ringleinknorr.boulderapp.routes.Route;
+import io.objectbox.annotation.Convert;
 import io.objectbox.annotation.Entity;
 import io.objectbox.annotation.Id;
+import io.objectbox.converter.PropertyConverter;
 import io.objectbox.relation.ToOne;
 
 @Entity
 public class SessionRoute {
     @Id
     private long id;
+    @Convert(converter = ResultConverter.class, dbType = Integer.class)
+    private Result result;
     private ToOne<Route> route;
     private ToOne<Session> session;
 
     public SessionRoute() {
+        result = Result.UNKNOWN;
     }
 
     public SessionRoute(long routeId, long sessionId) {
+        this();
         this.route.setTargetId(routeId);
         this.session.setTargetId(sessionId);
     }
 
     public SessionRoute(Route route, Session session) {
+        this();
         this.route.setTarget(route);
         this.session.setTarget(session);
     }
@@ -48,4 +57,42 @@ public class SessionRoute {
     public void setSession(ToOne<Session> session) {
         this.session = session;
     }
+
+    public Result getResult() {
+        return result;
+    }
+
+    public void setResult(@NonNull Result result) {
+        this.result = result;
+    }
+
+    public enum Result {
+        SUCCESS(0), FAILURE(1), UNKNOWN(2);
+
+        final int id;
+
+        Result(int id) {
+            this.id = id;
+        }
+    }
+
+    public static class ResultConverter implements PropertyConverter<Result, Integer> {
+        @Override
+        public Result convertToEntityProperty(Integer databaseValue) {
+            if (databaseValue != null) {
+                for (Result result : Result.values()) {
+                    if (result.id == databaseValue) {
+                        return result;
+                    }
+                }
+            }
+            return Result.UNKNOWN;
+        }
+
+        @Override
+        public Integer convertToDatabaseValue(Result entityProperty) {
+            return entityProperty == null ? null : entityProperty.id;
+        }
+    }
+
 }
