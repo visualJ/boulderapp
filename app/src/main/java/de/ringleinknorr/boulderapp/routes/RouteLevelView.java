3f76c0bbe8b038567633f39ea4ad023af0e41f20
Jonas Knorr <sieg.tronje@gmail.com>
2018-06-24 21:06:01 +0200

    Add route level selection marker

--- app/src/main/java/de/ringleinknorr/boulderapp/routes/RouteLevelView.java
+++ app/src/main/java/de/ringleinknorr/boulderapp/routes/RouteLevelView.java
@@ -1,6 +1,9 @@
 package de.ringleinknorr.boulderapp.routes;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.constraint.ConstraintLayout;
 import android.support.constraint.Constraints;
 import android.util.AttributeSet;
@@ -9,6 +12,7 @@ import android.view.LayoutInflater;
 import android.widget.Button;
 import android.widget.TextView;
 
+import butterknife.BindColor;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -20,20 +24,47 @@ public class RouteLevelView extends ConstraintLayout implements SelectableItemLi
     @BindView(R.id.level_text)
     TextView routeLevelText;
 
+    @BindColor(R.color.cardview_dark_background)
+    int darkGrey;
+    @BindColor(R.color.cardview_light_background)
+    int lightGrey;
+    @BindColor(R.color.colorPrimary)
+    int green;
+
     private boolean isSelected = false;
     private SelectableItemListAdapter.OnSelectedListener onSelectedListener = selected -> {
     };
+    private Paint p;
 
     public RouteLevelView(Context context, AttributeSet attrs) {
         super(context, attrs);
         LayoutInflater.from(context).inflate(R.layout.view_route_level, this);
         ButterKnife.bind(this);
         setLayoutParams(new Constraints.LayoutParams(Constraints.LayoutParams.MATCH_PARENT, Constraints.LayoutParams.WRAP_CONTENT));
+        p = new Paint(Paint.ANTI_ALIAS_FLAG);
+        p.setStyle(Paint.Style.STROKE);
+    }
+
+    @Override
+    public void dispatchDraw(Canvas canvas) {
+        super.dispatchDraw(canvas);
+        int offsetTextViewX = routeLevelText.getLeft();
+        int offsetTextViewY  = routeLevelText.getTop();
+
+        if (isSelected) {
+            p.setStrokeWidth(8);
+            p.setColor(green);
+        } else {
+            p.setStrokeWidth(4);
+            p.setColor(darkGrey);
+        }
+        canvas.drawRect(offsetTextViewX, offsetTextViewY, routeLevelText.getMeasuredWidth()+offsetTextViewX, routeLevelText.getMeasuredWidth()+offsetTextViewY, p);
     }
 
     @OnClick(R.id.level_text)
-    public void onLevelSelection(){
+    public void onLevelSelection() {
         isSelected = !isSelected;
+        invalidate();
         onSelectedListener.onSelected(isSelected);
     }
 
