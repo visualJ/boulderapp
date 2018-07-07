6118a8bf918652da227c0e1ba39f04b1b8f948c1
visualJ <benedikt-ringlein@hotmail.de>
2018-07-07 12:22:42 +0200

    Add animation to route level selection

--- app/src/main/java/de/ringleinknorr/boulderapp/routes/RouteLevelView.java
+++ app/src/main/java/de/ringleinknorr/boulderapp/routes/RouteLevelView.java
@@ -6,7 +6,10 @@ import android.support.constraint.Constraints;
 import android.support.v7.widget.CardView;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.OvershootInterpolator;
 
+import butterknife.BindInt;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -15,11 +18,24 @@ import de.ringleinknorr.boulderapp.SelectableItemListAdapter;
 
 public class RouteLevelView extends ConstraintLayout implements SelectableItemListAdapter.Selectable {
 
+    public static final int SELECTED_ELEVATION = 12;
+    public static final float SELECTED_SCALE = 1.2f;
+    public static final float DESELECTED_SCALE = 1f;
+    public static final float SELECTED_ALPHA = 1f;
+    public static final int DESELECTED_ELEVATION = 4;
+    public static final float DESELECTED_ALPHA = 0.6f;
+
     @BindView(R.id.level_selection_view)
     CardView levelSelectionCardView;
+
+    @BindInt(android.R.integer.config_shortAnimTime)
+    int mShortAnimationDuration;
+
     private boolean isSelected = false;
     private SelectableItemListAdapter.OnSelectedListener onSelectedListener = selected -> {
     };
+    private OvershootInterpolator overshootInterpolator = new OvershootInterpolator(5f);
+    private DecelerateInterpolator decelerateInterpolator = new DecelerateInterpolator(2f);
 
     public RouteLevelView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -40,21 +56,31 @@ public class RouteLevelView extends ConstraintLayout implements SelectableItemLi
     @OnClick(R.id.level_selection_view)
     public void onLevelSelection() {
         isSelected = !isSelected;
-        updateUI();
+        animateUI();
         onSelectedListener.onSelected(isSelected);
+    }
+
+    private void animateUI() {
+        if (isSelected) {
+            levelSelectionCardView.setCardElevation(SELECTED_ELEVATION);
+            levelSelectionCardView.animate().scaleX(SELECTED_SCALE).scaleY(SELECTED_SCALE).alpha(SELECTED_ALPHA).setDuration(mShortAnimationDuration).setInterpolator(overshootInterpolator);
+        } else {
+            levelSelectionCardView.setCardElevation(DESELECTED_ELEVATION);
+            levelSelectionCardView.animate().scaleX(DESELECTED_SCALE).scaleY(DESELECTED_SCALE).alpha(DESELECTED_ALPHA).setDuration(mShortAnimationDuration).setInterpolator(decelerateInterpolator);
+        }
     }
 
     private void updateUI() {
         if (isSelected) {
-            levelSelectionCardView.setCardElevation(12);
-            levelSelectionCardView.setScaleX(1.2f);
-            levelSelectionCardView.setScaleY(1.2f);
-            levelSelectionCardView.setAlpha(1f);
+            levelSelectionCardView.setCardElevation(SELECTED_ELEVATION);
+            levelSelectionCardView.setScaleX(SELECTED_SCALE);
+            levelSelectionCardView.setScaleY(SELECTED_SCALE);
+            levelSelectionCardView.setAlpha(SELECTED_ALPHA);
         } else {
-            levelSelectionCardView.setCardElevation(4);
-            levelSelectionCardView.setScaleX(1f);
-            levelSelectionCardView.setScaleY(1f);
-            levelSelectionCardView.setAlpha(0.6f);
+            levelSelectionCardView.setCardElevation(DESELECTED_ELEVATION);
+            levelSelectionCardView.setScaleX(DESELECTED_SCALE);
+            levelSelectionCardView.setScaleY(DESELECTED_SCALE);
+            levelSelectionCardView.setAlpha(DESELECTED_ALPHA);
         }
         invalidate();
     }
